02bad767540cf564c7659828ba3dbd5660ee24ea
Almir <167138187+AlmirSai@users.noreply.github.com>
2025-01-09 18:23:52 +0300

    Initial MVP configuration

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,46 @@
+// cmd/sso/main.go
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	// TEMP solution
+	fmt.Println("Config:", cfg)
+	fmt.Println("Logger:", log)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
